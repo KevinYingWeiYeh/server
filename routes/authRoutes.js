c40c0b14223a6b4b8ca70f1e53c1d70f04b9729e
Kevin Yeh <revinyeh@gmail.com>
2019-03-25 23:55:17 -0700

    add logout router for logout usage

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,12 @@ module.exports = app => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  // for testing usage
   app.get("/api/currect_user", (req, res) => {
     res.send(req.user);
   });
