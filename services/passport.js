47f1fa6769f8641101bd69ffcea6421899ec0a72
Kevin Yeh <revinyeh@gmail.com>
2019-03-25 23:44:13 -0700

    added cookie-section for passport to handle cookies and sections

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +23,18 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      // eslint-disable-next-line no-console
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // We don't save userId if the Id is already excisting
+          // eslint-disable-next-line no-console
+          console.log(`the user ${profile.displayName} is already excisting`);
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
