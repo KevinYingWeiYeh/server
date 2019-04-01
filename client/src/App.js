30f2617477c57198b01bc1121458bb1142d2cee5
Kevin Yeh <revinyeh@gmail.com>
2019-04-01 15:16:15 -0700

    add proxy in client side for production

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <a href="http://localhost:5000/auth/google">Sign In With Google</a>
         </header>
       </div>
     );
