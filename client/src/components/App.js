45eec5b74e047e5614356997f3bc6b908ebcc6c6
Kevin Yeh <revinyeh@gmail.com>
2019-04-02 11:53:06 -0700

    added materialize css lab into project and style header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Header from "./Header";
+
 const Landing = () => <h2> Landing </h2>;
 const Dashboard = () => <h2> Dashboard </h2>;
 const SurveryNew = () => <h2> SurveryNew </h2>;
-const Header = () => <h2> Header </h2>;
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
