e5e0af03853880c6d97d12fd60394df04ea30683
Kevin Yeh <revinyeh@gmail.com>
2019-04-02 01:15:58 -0700

    added component example into App.js component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Landing = () => <h2> Landing </h2>;
+const Dashboard = () => <h2> Dashboard </h2>;
+const SurveryNew = () => <h2> SurveryNew </h2>;
+const Header = () => <h2> Header </h2>;
 
 const App = () => {
-  return <div>Hi there</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SurveryNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
