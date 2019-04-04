a01629147672966a4e430ee47ae958fbc949bea1
Kevin Yeh <revinyeh@gmail.com>
2019-04-03 23:00:51 -0700

    add new proxy logout route in to setupProxy file

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy("/api/current_user", { target: "http://localhost:5000/" }),
-    proxy("/auth/google", { target: "http://localhost:5000/" })
+    proxy("/auth/google", { target: "http://localhost:5000/" }),
+    proxy("/api/logout", { target: "http://localhost:5000/" })
   );
 };
