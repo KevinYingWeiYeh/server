8c254221c9963f35578d7a51dc98851fad4c3510
Kevin Yeh <revinyeh@gmail.com>
2019-04-05 23:24:29 -0700

    added bodyParser dependence on server side

--- routes/billingRoute.js
+++ routes/billingRoute.js
@@ -1,3 +1,18 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
 module.exports = app => {
-  app.post("/aip/stripe", (req, res) => {});
+  app.post("/aip/stripe", (req, res) => {
+    stripe.charges.create(
+      {
+        amount: 2000,
+        currency: "usd",
+        source: "tok_visa", // obtained with Stripe.js
+        description: "Charge for jenny.rosen@example.com"
+      },
+      function(err, charge) {
+        // asynchronously called
+      }
+    );
+  });
 };
