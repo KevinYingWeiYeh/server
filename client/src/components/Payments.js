55d8756d148431bcdc207d862392a8f7bf76322f
Kevin Yeh <revinyeh@gmail.com>
2019-04-05 17:04:15 -0700

    wired stripe with handleToken action

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,8 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
 
 class Payments extends React.Component {
   render() {
@@ -8,7 +11,7 @@ class Payments extends React.Component {
         name="Emaily"
         description="$5 for 5 Email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add credits</button>
@@ -17,4 +20,7 @@ class Payments extends React.Component {
   }
 }
 
-export default Payments;
+export default connect(
+  null,
+  actions
+)(Payments);
