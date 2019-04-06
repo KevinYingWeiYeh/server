39d786a2c3c8cc7334dbb8ba5243a44b42460ea8
Kevin Yeh <revinyeh@gmail.com>
2019-04-05 23:16:53 -0700

    added new dependence stripe with a new server route billingRoute for dealing with the chargedata from fornt-part

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 // eslint-disable-next-line no-console
---
+++ routes/billingRoute.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+  app.post("/aip/stripe", (req, res) => {});
+};
