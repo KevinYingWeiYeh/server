6fb35af0006da5abb2f834b3aca0d10b8e442b38
Kevin Yeh <revinyeh@gmail.com>
2019-04-07 00:19:47 -0700

    add cradit points on Header component

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.Node_ENV === "production") {
+  app.use(express.static("client/build"));
+  //if express un production mode and couldn't find cliend/build file
+  const path = require("path");
+  app.got("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 // eslint-disable-next-line no-console
 console.log("Server start listening port:" + PORT);
