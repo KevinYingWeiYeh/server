630cc8575d151acd552692f388722713e83e2550
Kevin Yeh <revinyeh@gmail.com>
2019-04-08 23:50:00 -0700

    test commit for heroku deply

---
+++ client/build/precache-manifest.b42a9d49d5c89fe41f3b619b4212bcb7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64ed65853ba78a19e38",
+    "url": "/static/js/main.546720b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a924450c7d5e1342bfa",
+    "url": "/static/js/2.7f296459.chunk.js"
+  },
+  {
+    "revision": "37082e59d7495b24761594f0f6f486a2",
+    "url": "/index.html"
+  }
+];
